a51d62dcf94815cb65e7940590d1f0e9f013f077
Marios Sioutis <smarios@jaist.ac.jp>
2020-07-02 09:13:21 +0900

    Add http testing & benchmarking

---
+++ wget/bench_web_single.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#http --ignore-stdin :8088/elapi/v1/devices/150.65.230.105:001101/echoCommands request:=@er5 &
+http --ignore-stdin :8088/elapi/v1/devices/150.65.230.104:001101/echoCommands request:=@er5 &
+wait
